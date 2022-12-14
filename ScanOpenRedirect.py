f9f4450d5e870efcee708a9af960d7103da86acc
p0ch4t <70673234+p0ch4t@users.noreply.github.com>
2022-12-14 15:57:24 -0300

    Update ScanOpenRedirect.py

--- ScanOpenRedirect.py
+++ ScanOpenRedirect.py
@@ -9,8 +9,8 @@ def validaciones(url, parametros_url, redirect_url, payload):
 	r = requests.get(url, params=params)
 	try:
 		if "pong" in r.text and r.headers['host-header'] == '8441280b0c35cbc1147f8ba998a563a7':
-			return url+'?'+params
-		if "https:\u002F\u002Fpfelilpe.com\u002Fping" in r.text:
+			print(url+'?'+params)
+		if "pfelilpe.com\u002Fping" in r.text:
 			with open('otros_posibles_dom_open_redirect.txt'):
 				print(url+'?'+params+" --> REVISAR")
 		else:
@@ -19,16 +19,14 @@ def validaciones(url, parametros_url, redirect_url, payload):
 		return ""
 
 def scan_open_redirect(lista_urls):
-	payloads = ['=https://pfelilpe.com/ping?', '=//pfelilpe.com/ping?', 
-'=pfelilpe.com/ping','=\/\/pfelilpe.com/ping?', '=https:pfelilpe.com/ping?']
+	payloads = ['=https://pfelilpe.com/ping?', '=//pfelilpe.com/ping?', '=pfelilpe.com/ping','=\/\/pfelilpe.com/ping?', '=https:pfelilpe.com/ping?']
 	for url in lista_urls:
 		try:
 			parametros_url = url.split('?')[1]
 			url = url.split('?')[0]
 			redirect_url = search(r'(%253D|%3D|=)http(s|)(%253A|%3A|:)(%252F|%2F|\/)(%252F|%2F|\/)[A-Za-z0-9-]+\.', parametros_url).group(0)
 			for payload in payloads:
-				url_maliciosa = validaciones(url, parametros_url, redirect_url, payload)
-				print(url_maliciosa)
+				validaciones(url, parametros_url, redirect_url, payload)
 		except Exception as e:
 			pass
 
